dfeac134dcf3e97e096a868b339c9ce89fbaa60d
DaniloTsurkan <126474745+DaniloTsurkan@users.noreply.github.com>
2025-01-23 13:06:46 +0200

    Завершено блок умови оренди

--- js/script.js
+++ js/script.js
@@ -11,3 +11,8 @@ if (iconMenu) {
 		menuHeader.classList.toggle('active');
 	});
 }
+
+
+window.addEventListener('scroll', function () {
+	scrollY > 0 ? document.querySelector('.header').classList.add('scroll') : document.querySelector('.header').classList.remove('scroll');
+})
