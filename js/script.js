38615dc5b83e14b62454eb4fb273d1e85f5c58ca
DaniloTsurkan <126474745+DaniloTsurkan@users.noreply.github.com>
2025-01-31 13:52:22 +0200

    Завершення верстки

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ if (iconMenu) {
 window.addEventListener('scroll', function () {
 	scrollY > 0 ? document.querySelector('.header').classList.add('scroll') : document.querySelector('.header').classList.remove('scroll');
 })
+
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	loop: true,
+	slidesPerView: 3,
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	spaceBetween: 20,
+	breakpoints: {
+		1024: { slidesPerView: 3 },
+		768: { slidesPerView: 2 },
+		320: { slidesPerView: 1 },
+	},
+});
